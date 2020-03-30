01e47de147639c4cc2e941311ea5242fa857ded4
github username <github email address>
2020-03-30 18:00:04 -0400

    I thought I'd already staged, committed and pushed this, but I need to try again, for some reason: Now, a prayer cannot be submitted without a title and a body. Also, while showing it to Valerie, I realized that sometimes a newly created prayer was loading, and sometimes it wasn't, so I simply added another '.then(new Prayers())' to the createPrayer() method in the UserAdapter class, and that seemed to do the trick.

--- prayer-warriors-js-frontend/src/adapters/UserAdapter.js
+++ prayer-warriors-js-frontend/src/adapters/UserAdapter.js
@@ -8,22 +8,25 @@ class UserAdapter {
     let title = document.getElementById("new-prayer-title").value
     let body = document.getElementById("new-prayer-body").value
     //fetch('http://localhost:3000/api/v1/users', {
-    fetch('http://localhost:3000/api/v1/prayers', {
-      method: "POST",
-      headers: {"Content-TYPE": "application/json"},
-      body: JSON.stringify({
-        title: `${title}`,
-        body: `${body}`,
-        amens: 0,
-        outcome: `${" "}`,
-        user_id: `${loggedInUser['id']}`
+    if (title && body) {
+      fetch('http://localhost:3000/api/v1/prayers', {
+        method: "POST",
+        headers: {"Content-TYPE": "application/json"},
+        body: JSON.stringify({
+          title: `${title}`,
+          body: `${body}`,
+          amens: 0,
+          outcome: `${" "}`,
+          user_id: `${loggedInUser['id']}`
+        })
       })
-    })
-    .then(response => response.json())
-    .then(jsonizedResponse => console.log(jsonizedResponse))
-    .then(document.getElementById("new-prayer-title").value = "")
-    .then(document.getElementById("new-prayer-body").value = "")
-    .then(new Prayers())
+      .then(response => response.json())
+      .then(jsonizedResponse => console.log(jsonizedResponse))
+      .then(document.getElementById("new-prayer-title").value = "")
+      .then(document.getElementById("new-prayer-body").value = "")
+      .then(new Prayers())
+      .then(new Prayers())
+    }
   }
 }
 //   constructor() {
